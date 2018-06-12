bb86e1de3b5f0397bce45af81256e1edd68c1ce4
jatashankar <jspatel08@gmail.com>
2018-06-12 13:29:32 +0530

    Gitacora saved 1 changed files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  userData:any;
+  userData: any;
   title = 'CTP App:';
   searchKey = 'hello';
-
-  userName:any[] = ['Ram', 'Shyam','Mohan','Robert'];
-
-  this.userData=[
-    {name:'Ram',age:'25',city:'Delhi'},
-    {name:'Sohan',age:'35',city:'Noida'},
-    {name:'Sita',age:'20',city:'Ajmer'},
-    {name:'RamDas',age:'55',city:'UP'},
-    {name:'Rocky',age:'30',city:'Mumbai'}
+  userName: any[] = ['Ram', 'Shyam', 'Mohan', 'Robert'];
+  userDataFun() {
+    this.userData = [
+      { name: 'Ram', age: '25', city: 'Delhi' },
+      { name: 'Sohan', age: '35', city: 'Noida' },
+      { name: 'Sita', age: '20', city: 'Ajmer' },
+      { name: 'RamDas', age: '55', city: 'UP' },
+      { name: 'Rocky', age: '30', city: 'Mumbai' }
     ]
-  
-
+  }
 }
