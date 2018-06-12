d2992f1ab8516f4975748eaa69ca9b08a8063afb
jatashankar <jspatel08@gmail.com>
2018-06-12 16:31:56 +0530

    Gitacora saved 1 changed files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  constructor(){
+  }
   userData: any;
   title = 'CTP App:';
   searchKey = 'hello';
