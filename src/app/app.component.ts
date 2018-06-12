454f4638926331577dc70b982790f9867f45ad5b
jatashankar <jspatel08@gmail.com>
2018-06-12 13:27:34 +0530

    Gitacora saved 1 changed files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
 
   userName:any[] = ['Ram', 'Shyam','Mohan','Robert'];
 
-  this.userDate:=
+  this.userData:=
     [
     {name:'Ram',age:'25',city:'Delhi'},
     {name:'Sohan',age:'35',city:'Noida'},
